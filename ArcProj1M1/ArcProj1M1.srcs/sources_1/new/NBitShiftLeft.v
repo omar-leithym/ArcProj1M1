7c10c5cf6261e11ac89a2e5012e8aecc3983f602
Ahmed Ayman Elkhodary <126615749+aae121@users.noreply.github.com>
2025-04-09 23:50:32 +0200

    Update NBitShiftLeft.v

--- ArcProj1M1/ArcProj1M1.srcs/sources_1/new/NBitShiftLeft.v
+++ ArcProj1M1/ArcProj1M1.srcs/sources_1/new/NBitShiftLeft.v
@@ -1,7 +1,24 @@
 `timescale 1ns / 1ps
-module NBitShiftLeft #(parameter N=32) (
-    input  wire [N-1:0] in,
-    output wire [N-1:0] out
+module NBitShiftLeft(
+    input  wire [31:0] in,           
+    input  wire [31:0] a,            
+    input  wire [4:0]  shamt,        
+    input  wire [1:0]  type,         
+    output wire [31:0] out,         
+    output reg  [31:0] r             
 );
+    
     assign out = in << 1;
+    
+    wire signed [31:0] signed_a = $signed(a);
+    
+    always @ (*) begin
+        case(type)
+            2'b00: r = a << shamt;                  
+            2'b01: r = a >> shamt;                  
+            2'b10: r = $signed(signed_a) >>> shamt; 
+            default: r = a;
+        endcase
+    end
+
 endmodule
