3200e63eabf60586f411b1cfbdf394e2f9f1b0bd
Ahmed Ayman Elkhodary <126615749+aae121@users.noreply.github.com>
2025-04-09 23:51:47 +0200

    Update NBitMux2x1.v

--- ArcProj1M1/ArcProj1M1.srcs/sources_1/new/NBitMux2x1.v
+++ ArcProj1M1/ArcProj1M1.srcs/sources_1/new/NBitMux2x1.v
@@ -1,18 +1,18 @@
 `timescale 1ns / 1ps
 module NBitMux2x1 #(parameter N=8) (
-    input wire [N-1:0] A, 
-    input wire [N-1:0] B,
-    input wire S,
-    output wire [N-1:0] C
+    input wire [N-1:0] a, 
+    input wire [N-1:0] b,
+    input wire s,
+    output wire [N-1:0] c
 );
     genvar i;
     generate 
         for (i=0; i < N; i=i+1) begin
             mux2x1 myMux(
-                .A(A[i]), 
-                .B(B[i]), 
-                .S(S), 
-                .C(C[i])
+                .a(a[i]), 
+                .b(b[i]), 
+                .s(s), 
+                .c(c[i])
             );
         end
     endgenerate
